91385bae607fac5309646c53844e5d7855e55b65
pasolre95kim <pasolre95kim@gmail.com>
2018-10-29 16:47:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+//return new array containing first two drivers from passed in array
+const returnFirstTwoDrivers =
+  function(drivers) {
+  return drivers.slice(0,2)
+  }
+
+//return new array with last two drivers from passed in array
+const returnLastTwoDrivers =
+  function(drivers) {
+    return drivers.slice(-2)
+  }
+//has the returnFirstTwoDrivers function to as its first element
+// has returnLastTwoDrivers as last element
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//returns a function
+//should mutiply a given value using the created multiplier
+ function createFareMultiplier(multiply){
+  return function(integer) {
+    return multiply * integer;
+    }
+  }
+
+  const fareDoubler = function(createFareMultiplier) {
+      return createFareMultiplier * 2
+  }
+
+  const fareTripler = function(createFareMultiplier) {
+    return createFareMultiplier * 3
+  }
+
+  function fetchSpecifiedDrivers(drivers, func) {
+    return func(drivers)
+  }
